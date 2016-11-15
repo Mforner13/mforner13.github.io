fc36d329219a176c18e783367f83da2346ff56f2
caro1432 <co6446cz@my.bpp.com>
2016-11-15 09:26:43 +0000

    Add files via upload
    :)

---
+++ Final/js/myjQuery.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	$(this).find("ul.dropdown-menu").hide();
+	$("li.drpdwn-menu").hover(function(){
+		$(this).find("ul.dropdown-menu").show(100);
+	}, function() {
+		$(this).find("ul.dropdown-menu").hide(300);
+	});
+	$("a.menu-link").hover(function(){
+		$(this).css("color", "#304155");
+	}, function() {
+		$(this).css("color", "#e6f3ff");
+	});
+});
+
+
+$(document).ready(function(){
+  $('.bxslider').bxSlider();
+});
